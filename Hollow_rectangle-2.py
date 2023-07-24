3d216acb92fd9b7130db6af04dab8758f68f8eb9
balaji <76476394+Balaji041@users.noreply.github.com>
2023-07-24 16:09:01 +0530

    Create Hollow_rectangle-2.py

---
+++ Hollow_rectangle-2.py
@@ -0,0 +1,17 @@
+M=int(input())
+N=int(input())
+print("+"+"-"*N+"+")
+for i in range(M):
+    hallow_space=" "*N
+    print("|"+hallow_space+"|")
+print("+"+"-"*N+"+")
+"""
+input:3
+input:10
+output:
++----------+
+|          |
+|          |
+|          |
++----------+
+"""
