d24457d408abca81d701e0953f9f83e84d9c57a0
balaji <76476394+Balaji041@users.noreply.github.com>
2023-07-26 16:00:39 +0530

    Update Numbers_in _pyramid.py

--- Numbers_in _pyramid.py
+++ Numbers_in _pyramid.py
@@ -1,9 +1,20 @@
+S=int(input())
 N=int(input())
 for i in range(1,N+1):
     string=""
-    for j in range(N):
-        string=string+str(N-j)+" "
-    print(string)
+    Sr=S
+    left_space=" "*(N-i)
+    for j in range(1,i+1):
+        string+=str(Sr)+" "
+        Sr+=1
+    print(left_space+string)
+        
+    
+    
+   
+        
+        
+    
 """
 input:7 5
 output:7 
